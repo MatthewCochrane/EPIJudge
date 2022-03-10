e507e2fd2aed8624e74d34a06d74ba69b0fb28c1
Matthew Cochrane <matthew.cochrane.eng@gmail.com>
2022-03-11 09:42:57 +1000

    First problem in chapter 5.

--- epi_judge_python/even_odd_array.py
+++ epi_judge_python/even_odd_array.py
@@ -8,8 +8,63 @@ from test_framework.test_utils import enable_executor_hook
 
 
 def even_odd(A: List[int]) -> None:
-    # TODO - you fill in here.
-    return
+    """
+    Input is an array of integers.  Reorder them so that the even entries appear first.
+    It doesn't specify if we should preserver the ordering apart from this.
+
+    Example 1:
+        [1,2,3,4,5,6]
+        [1,2,5,2,4,6]
+
+    Example 2:
+        [1,2]
+        [1,2]
+
+    Example 3:
+        [2,1]
+        [1,2]
+
+    Example 4:
+        [1,1,1]
+        [1,1,1]
+
+    Negative numbers aren't special.
+
+    Approach 1:
+        Sort the entries by odd/evenness
+        O(n log n) time
+        O(n) space
+
+    Approach 2:
+        Iterate through the array and swap positions.
+
+        [2,1,3,3]
+         | |
+        [4,3,2,1]
+           | |
+
+        while l < r
+            if l is even, increment l
+            if r is odd, increment r
+            if neither of those, then l is odd and r is even, so swap l and r and increment both
+
+        O(n) time complexity
+        O(1) space complexity
+
+    Average and median time to run was the same in both approaches.  Timsort might be helping in approach 1.
+    """
+    l, r = 0, len(A) - 1
+    while l < r:
+        if A[l] % 2 == 0:
+            # is even
+            l += 1
+        elif A[r] % 2:
+            # is odd
+            r -= 1
+        else:
+            A[l], A[r] = A[r], A[l]
+            l += 1
+            r -= 1
 
 
 @enable_executor_hook
@@ -22,15 +77,17 @@ def even_odd_wrapper(executor, A):
     for a in A:
         if a % 2 == 0:
             if in_odd:
-                raise TestFailure('Even elements appear in odd part')
+                raise TestFailure("Even elements appear in odd part")
         else:
             in_odd = True
     after = collections.Counter(A)
     if before != after:
-        raise TestFailure('Elements mismatch')
+        raise TestFailure("Elements mismatch")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     exit(
-        generic_test.generic_test_main('even_odd_array.py',
-                                       'even_odd_array.tsv', even_odd_wrapper))
+        generic_test.generic_test_main(
+            "even_odd_array.py", "even_odd_array.tsv", even_odd_wrapper
+        )
+    )
