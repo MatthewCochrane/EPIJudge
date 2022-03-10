142c61b07f6c5d8a493601934046912f6e333818
Matthew Cochrane <matthew.cochrane.eng@gmail.com>
2022-03-10 10:30:32 +1000

    Primitive multiply and divide questions. First time I've really understood how the divide question should be done!  Pretty awesome!  I should definitely repeat the divide question. The multiply question wasn't hard but was fairly detailed and probably took me too long (about 45 mins).  I did have to build an adder as well as the divide circuitry.

--- epi_judge_python/primitive_divide.py
+++ epi_judge_python/primitive_divide.py
@@ -2,11 +2,126 @@ from test_framework import generic_test
 
 
 def divide(x: int, y: int) -> int:
-    # TODO - you fill in here.
-    return 0
+    """
+    To divide two numbers using only addition, subtraction and shifting operators.
+    I assume we can use binary and bitwise operators too?  Maybe not...
+    We only need to find the quotient, not the remainder.
+
+    Eg 1.
+    10 / 2 = 5
+    0b1010
+    0b0010
+
+    How many times does 2 go into 10?
+    We can do repeated subtraction...
+    while x > y:
+        x -= y
+        result += 1
+    return result
+
+    This has very poor time complexity O(n) where n is the value of x.  That's O(2^bits) where bits is the number of bits.
+
+    We can probably do long division, or something similar to how we do multiplication...
+
+    For example
+    0b1010
+    0b0010 /
+    ======
+    Because the dividend is a power of 1 we a really just doing a shift on the original
+             100
+    0b11 |0b1100
+
+    So we say
+    How many times does 0b11 go into 0b1 ? 0
+    How many times does 0b11 go into 0b11 ? 1
+    What's left over? 0
+
+    Another
+               11
+    0b11 | 0b1001
+               11
+    0b11 goes into 1 - 0
+    0b11 goes into 10 - 0
+    0b11 goes into 100 - 1 with 1 remainder
+    0b11 goes into 11 - 1 time with 0 remainder
+
+    So the idea is: find the largest number we can fit into x
+    try fitting y into x, try fitting 2y into x
+    try 4y, try... 2^ny and find the largest y
+
+    For example, the one above...
+    0b11 does fit into 0b1001
+    0b110 does fit into 0b1001
+    0b1100 does not fit into 0b1001
+    so we use 0b110, and subtract it from
+    0b1001
+    0b0110 -
+    0b0011
+
+    Then we do the same thing with the left over.
+    0b0011 / 0b11
+
+    while x:
+        shift the denominator until it's larger than the numerator O(n) shifts max where n is number of bits
+        shift the denominator back one
+        set the bit in result corresponding to the number of times we shifted
+        subtract the shifted denominator from the numerator
+
+    The only problem with this is that we keep scanning from left to right to find the right number of shifts.
+    We can improve on this by finding the largest number of shifts the first times and decreasing it instead
+
+    shift the denominator until it's larger than the numerator
+    while x:
+        shift the denominator right until it's smaller than the numerator
+        set the bit in result that corresponds to the number of shifts
+        subtract the shifted denominator from the numerator
+
+    Example
+    0b1111 / 0b101 = 0b11 (15 / 5 = 3)
+
+    shift the denominator until it's larger than the numerator
+    0b101, 0b1010, 0b10100 (2 shifts)
+    while x: (x = 0b1111)
+        shift y right until smaller than x
+        0b1010 (now and 1 shift overall)
+        set bit in result
+        result = 0b10
+        subtract 0b1010 from 0b1111 (15 - 10 = 5)
+        x = 0b101
+    while x: (x=0b101)
+        shift y right until smaller than x
+        0b1010 -> 0b101 (not 0 shifts overall)
+        set bit in result
+        result = 0b11
+        subtract 0b101 from 0b101 (5 - 5 = 0)
+        x = 0
+    return 0b11
+
+    Looks good
+    Since we scan from right to left then back from left to right once this is O(n) where n is the number of bits.
+
+    """
+    set_mask = 1
+    result = 0
+    # shift the denominator until it's larger than the numerator
+    while y <= x:
+        y <<= 1
+        set_mask <<= 1
+    while x:
+        # shift the denominator right until it's smaller than the numerator
+        while y > x:
+            y >>= 1
+            set_mask >>= 1
+        # set the bit in result that corresponds to the number of shifts
+        result |= set_mask
+        # subtract the shifted denominator from the numerator
+        x -= y
+    return result
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     exit(
-        generic_test.generic_test_main('primitive_divide.py',
-                                       'primitive_divide.tsv', divide))
+        generic_test.generic_test_main(
+            "primitive_divide.py", "primitive_divide.tsv", divide
+        )
+    )
--- epi_judge_python/primitive_multiply.py
+++ epi_judge_python/primitive_multiply.py
@@ -2,8 +2,152 @@ from test_framework import generic_test
 
 
 def multiply(x: int, y: int) -> int:
-    # TODO - you fill in here.
-    return 0
+    """
+    Start: 20:45
+    Write a function to multiply two numbers.
+    Allowed instructions:
+    assignment
+    >>, <<, |, &, ~, ^
+    ==
+    So you can't use addition, subtraction, multiplication, etc.
+    You can use loops.  But how can you use loops if you can't use addition or lt / gt?
+
+    How do we multiply two numbers?
+
+    Example 1
+    3*4 = 12
+    0b0011
+    0b0100
+    ======
+    0b1100
+
+    Example 2
+    3*2 = 6
+    0b0011
+    0b0010
+    ======
+    0b0110
+
+    So multiplying by a power of two is a shift of the original number
+
+    Example 3
+    3 * 3 = 9
+    0b0011
+    0b0011
+    ======
+    0b1001
+
+    Intuitively this is
+
+    0b0011
+    0b0001 *
+    ------
+    0b0011
+    Plus
+    0b0011
+    0b0010 *
+    ------
+    0b0110
+
+    so
+    0b0011
+    0b0110 +
+    ======
+    0b1001
+
+    Ok.  But we're not allowed to use addition!
+    Which means we need to implement addition in bitwise operators as well!
+
+    Let's summarise this part, then we can build an adder.
+
+    to multiply a * b
+    result = 0
+    while b:
+        if b & 1:
+            result = add(result, a)
+        a <<= 1
+        b >>= 1
+    return result
+
+    This is O(bits in b * complexity of add)
+
+    Adder:
+    To add two numbers
+
+    Ex 1
+    3 + 4 = 7
+    0b011
+    0b100 +
+    0b111
+
+    Ex 2
+    7 + 7 = 14
+    0b0111
+    0b0111 +
+    0b1110
+
+    You can't really do addition in less than O(n)
+
+    Carry is true if 2 or more are 1
+    a b c res
+    0 0 0 0
+    0 0 1 0
+    0 1 0 0
+    0 1 1 1
+    1 0 0 0
+    1 0 1 1
+    1 1 0 1
+    1 1 1 1
+
+    if (a & b) | (b & c) | (a & c)
+
+    Basically
+    a + b
+    mask = 1
+    result = 0
+    carry = 0
+    while a | b | carry:
+        # boolean operators
+        result |= (a & mask) ^ (b & mask) ^ carry
+        carry = (a & b) | (b & c) | (a & c)
+        a >>= 1
+        b >>= 1
+        mask <<= 1
+    return result
+
+
+
+    Overall time complexity is O(n^2)
+    Overall space complexity is O(1)
+    Can we improve on this?
+    Yes, if we can use a hardware adder.  It would drop to O(n).
+
+    Passed all tests at 21:27
+
+    """
+    def add(a: int, b: int) -> int:
+        mask = 1
+        result = 0
+        carry = 0
+        while a or b or carry:
+            a_bit = bool(a & 1)
+            b_bit = bool(b & 1)
+            bit = a_bit ^ b_bit ^ carry
+            if bit:
+                result |= mask
+            carry = (a_bit and b_bit) or (a_bit and carry) or (b_bit and carry)
+            a >>= 1
+            b >>= 1
+            mask <<= 1
+        return result
+
+    result = 0
+    while y:
+        if y & 1:
+            result = add(result, x)
+        x <<= 1
+        y >>= 1
+    return result
 
 
 if __name__ == '__main__':
