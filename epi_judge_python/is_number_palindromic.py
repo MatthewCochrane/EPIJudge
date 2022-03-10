85bd2a9ea63eeefadf31acf9fb3505153273029b
Matthew Cochrane <matthew.cochrane.eng@gmail.com>
2022-03-10 14:59:29 +1000

    A couple more problems.  Thought there might be a better answer to the uniform random generator but turns out my solution was the same as theirs.  I wasted about 20 minutes looking for a better solution.

--- epi_judge_python/is_number_palindromic.py
+++ epi_judge_python/is_number_palindromic.py
@@ -2,12 +2,158 @@ from test_framework import generic_test
 
 
 def is_palindrome_number(x: int) -> bool:
-    # TODO - you fill in here.
+    """
+    Start: 12:32
+    Can we do this without converting to a string?
+
+    Example 1:
+    0 -> true
+
+    Example 2:
+    -1 -> false
+
+    Example 3:
+    12321 -> true
+
+    Example 4:
+    1221 -> true
+
+    Example 5:
+    -431 -> false
+
+    Example 6:
+    100 -> false
+
+    Looks like all negative numbers are not palindromes because of the negative sign.
+
+    So we can determine what the digits are by dividing by 10 and taking the remainder.
+    1221 % 10 = 1
+    1221 // 10 = 122
+    122 % 10 = 2
+    122 // 10 = 12
+    12 % 10 = 2
+    12 // 10 = 1
+    1 % 10 = 1
+    1 // 10 = 0
+
+    That's good but it requires O(n) space and it's really just converting it into a string
+    ourselves.
+    How can we do this from both sides at the same time?
+    Like, how can we determine the most significant digit?
+
+    1221 % 1000 = 221
+    (1221 - 221) / 1000
+    221 % 100 = 21
+    (221 - 21) / 100 = 2
+    21 % 10 = 1
+    (21 - 1) / 10 = 2
+
+    But how do we know what the most significant digit is?
+    1221 // 10 = 100 * 10 = 1000
+
+    1000 // 10 = 100 * 10 = 1000
+
+    Ok, so lets try that
+
+    find the starting modulo
+    while the modulo > 0:
+        determine left remainder and left digit
+        l_digit = (l_rem - (l_rem % modulo)) / modulo
+        l_rem = l_rem % modulo
+        modulo //= 10
+        determine right remainder and right digit
+        r_digit = x % 10
+        x = x // 10
+        if left and right digits are not equal, return False
+    return True
+
+    212
+    modulo = 1
+    while modulo <= x: 1000 < 212
+        modulo *= 10
+    modulo //= 10 100
+    l_rem = x
+    while modulo > 0... 100 > 0
+        l_digit = (212 - (212 % 100)) / 100 = 212-12/100 = 200 / 100 = 2
+        l_rem = 212 % 100 = 12
+        modulo //= 10 = 10
+        r_digit = 212 % 10 = 2
+        x //= 10 = 21
+        l_digit == rdigit (2 == 2)
+    while modulo > 0... 10 > 0
+        l_digit = (12 - (12 % 10)) / 10 = 1
+        l_rem = 12 % 10 = 2
+        modulo //= 10 = 1
+        r_digit = 21 % 10 = 1
+        x //= 10 = 2
+        l_digit == r_digit (1 == 1)
+    Would be nice to exit now...
+
+    change the modulo thing...
+    iterations = 1
+    modulo = 1
+    while modulo <= x: # 1000 <= 212, its = 4
+        modulo *= 10
+        iterations += 1
+    modulo //= 10
+    # ensure we always check for overlap on the middle character
+    # if we have an odd length by not subtracting 1 from its here.
+    its //= 2
+    while iterations:
+        determine left remainder and left digit
+        l_digit = (l_rem - (l_rem % modulo)) / modulo
+        l_rem = l_rem % modulo
+        modulo //= 10
+        determine right remainder and right digit
+        r_digit = x % 10
+        x = x // 10
+        if left and right digits are not equal, return False
+        iterations -= 1
+
+    Time complexity: O(n) where n is number of digits
+    Space complexity: O(1)
+
+    Finish time: 13:09 ~ 37 mins.  Still too slow...
+    But again, good work, everything worked!
+    Ok, actually had a bug.  I forgot to add a line of code to 'shift' the modulo.
+    Finish: 13:15
+
+    Notes from reading the answer:
+    Maybe I could have just used log.  I didn't because I felt in these problems they don't want you to use
+    shortcut functions.  That makes no sense when I think about it though.  They never said not to use certain
+    functions.
+    """
+    if x < 0:
+        return False
+    if x == 0:
+        return True
+    iterations = 1
+    modulo = 1
+    while modulo <= x: # 100000 <= 74447, its = 6
+        modulo *= 10
+        iterations += 1
+    modulo //= 10 # 10000
+    # ensure we always check for overlap on the middle character
+    # if we have an odd length by not subtracting 1 from its here.
+    iterations //= 2 # 6//2 = 3
+    left_remainder = x
+    while iterations:
+        left_digit = (left_remainder - (left_remainder % modulo)) // modulo
+        left_remainder %= modulo
+        modulo //= 10
+        right_digit = x % 10
+        x //= 10
+        if left_digit != right_digit:
+            return False
+        iterations -= 1
     return True
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     exit(
-        generic_test.generic_test_main('is_number_palindromic.py',
-                                       'is_number_palindromic.tsv',
-                                       is_palindrome_number))
+        generic_test.generic_test_main(
+            "is_number_palindromic.py",
+            "is_number_palindromic.tsv",
+            is_palindrome_number,
+        )
+    )
--- epi_judge_python/uniform_random_number.py
+++ epi_judge_python/uniform_random_number.py
@@ -1,9 +1,12 @@
 import functools
 import random
+import math
 
 from test_framework import generic_test
 from test_framework.random_sequence_checker import (
-    check_sequence_is_uniformly_random, run_func_with_retries)
+    check_sequence_is_uniformly_random,
+    run_func_with_retries,
+)
 from test_framework.test_utils import enable_executor_hook
 
 
@@ -12,28 +15,173 @@ def zero_one_random():
 
 
 def uniform_random(lower_bound: int, upper_bound: int) -> int:
-    # TODO - you fill in here.
-    return 0
+    """
+    Start: 14:06
+    Using a random number generator that can generate either 1 or 0, create
+    chose a random number between lower bound and upper bound.
+    The distribution should be even! That means there should be an equal
+    probability of generating all values in the range.
+    Both upper and lower bounds are *included*.
+
+    Example:
+        lower = 0
+        upper = 1
+        here we can just call zero_one_random()
+
+    Example 2:
+        lower = 0
+        upper = 5
+        There are 6 valid values.  Each should have an even probability
+        000
+        001
+        010
+        011
+        100
+        101
+        110
+        Tricky, we can't quite just generate three bits from zero_one_random...
+        There are 8 possible values that could be produced and we can't evenly
+        distribute 8 things across 6 values. (8 doesn't divide evently into 6)
+        Another way could be to find the nearest power of two that the number
+        of items in the range divides evenly into.
+        eg. here we need 6 values.
+        8 / 6 ..
+        16 / 6
+        32 / 6 =
+        64 / 6 .. 66 is close
+        128 / 6
+        Actually, will 6 ever divide evenly into a power of 2?
+
+        0b110
+        We could use 8, then if we get a number more than 6 just 're-roll'.  This has bad
+        worst case performance but really bad performance is very unlikely.
+
+        Here there's a 6/8 or 3/4 chance that we get an answer on the first 'roll'
+        The worst case would be if we selected a number that's just larger than a power two.
+        Eg. 513 -> we'd need 10 bits, so there's about a 50% chance we don't hit a value between 0 and 513.
+        Chances are that we'd hit a value within a few rolls though.
+        But can we guarantee an answer every time without 're-rolling'?
+
+        Need the distribution to 'fit'.  So if you have 10 numbers, you can only select a power of 2...
+        0
+        1
+        2
+        3
+        4
+
+        5
+        6
+        7
+        8
+        9
+
+        if count is even...
+            split in two
+        if count is odd...
+
+        3 numbers
+
+        0
+        1
+        2
+
+        2, 4, 8, 16
+        2*x = 3
+        x = 3/2
+        Ohh, instead of 2*2*2 we want 2+2+2
+        So take three numbers, a, b and c.  Sum them to get a number between 0 and 2.  Use that result.
+
+        Ok, now what if we have a larger odd number
+        5
+
+        0 12.5
+        1 12.5
+        2 12.5
+        3 12.5
+        4 50%
+
+        divide by 2?
+        We actually can't split it in two...  Unless the ratios are right.
+        2/5 and 3/5
+        2*2+2
+        2*2 = 4 +
+
+        Time: 14:30 about 25 mins
+        In the interest of time let's do the re-rolling one
+
+        Pseudo code
+        find the size of the range (upper - lower + 1)
+        take log2 of size to find the number of bits we need ** floor and add one to log val?
+        generate a number with that many bits using the 0-1 random generator
+        if the generated number is >= the size, try again
+        add lower to the generated number (lowest generated number should be zero)
+        return the result
+
+        test:
+        lower = 5, upper = 10
+        find size of range = 10 - 5 + 1 = 6
+        floor(log2(6)) + 1 = 3 (2^3 = 8)
+        random = 0b110
+        6 >= size
+        random = 0b101
+        5 + 5 = 10
+        return 10
+
+    Time complexity is O(log n) where n is the range.
+    Space complexity is O(1) as long as we don't included the generated bits that are the result.
+
+    Time: 14:48
+    Took 42 minutes.
+    Hmm, this is ok but still a bit long.
+    Also, I should probably ask beforehand, how long do you expect this to take, so I know how fast to go,
+    when to search for more optimal solutions, etc.
+
+    Ok, I had an interesting bug with the walrus operator there!
+    doing `x := f() > y` is saying `x := (f(x) > y)` but I wanted `(x := f(x)) > y`.  Good one to be aware
+    of!  I don't use the walrus often.  It's actually pretty helpful too!  I would have had to call the
+    function in two places if I didn't use the walrus!
+
+    Ok, now that I'm done, can I think of a way to do this without the 're-rolls'?
+    Or can I prove that we can't find a number z such that 3*z = 2^z
+    3 = 2^z / z
+    log2(3z) = z
+    """
+    size = upper_bound - lower_bound + 1
+    bits = math.floor(math.log2(size)) + 1
+
+    def n_bit_random(n: int) -> int:
+        result = 0
+        for i in range(n):
+            if zero_one_random():
+                result |= 1 << i
+        return result
+
+    while (rand_num := n_bit_random(bits)) >= size:
+        pass
+    return rand_num + lower_bound
 
 
 @enable_executor_hook
 def uniform_random_wrapper(executor, lower_bound, upper_bound):
     def uniform_random_runner(executor, lower_bound, upper_bound):
         result = executor.run(
-            lambda:
-            [uniform_random(lower_bound, upper_bound) for _ in range(100000)])
+            lambda: [uniform_random(lower_bound, upper_bound) for _ in range(100000)]
+        )
 
         return check_sequence_is_uniformly_random(
-            [a - lower_bound for a in result], upper_bound - lower_bound + 1,
-            0.01)
+            [a - lower_bound for a in result], upper_bound - lower_bound + 1, 0.01
+        )
 
     run_func_with_retries(
-        functools.partial(uniform_random_runner, executor, lower_bound,
-                          upper_bound))
+        functools.partial(uniform_random_runner, executor, lower_bound, upper_bound)
+    )
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     exit(
-        generic_test.generic_test_main('uniform_random_number.py',
-                                       'uniform_random_number.tsv',
-                                       uniform_random_wrapper))
+        generic_test.generic_test_main(
+            "uniform_random_number.py",
+            "uniform_random_number.tsv",
+            uniform_random_wrapper,
+        )
+    )
