c8482957d2990e1e9b75a2b2e36f484726843912
Matthew Cochrane <matthew.cochrane.eng@gmail.com>
2022-03-11 15:58:33 +1000

    question 5.2

--- epi_judge_python/int_as_array_increment.py
+++ epi_judge_python/int_as_array_increment.py
@@ -4,8 +4,59 @@ from test_framework import generic_test
 
 
 def plus_one(A: List[int]) -> List[int]:
-    # TODO - you fill in here.
-    return []
+    """
+    Start: 15:30
+    Array of digits encoding a non-negative decimal integer and updates the array to
+    represent D + 1
+
+    Example
+        in: [1,2,9]
+        out: [1,3,0]
+
+    Example
+        [1]
+        [2]
+
+    Example
+        [9]
+        [1,0]
+
+    Example
+        [9,9,9]
+        [1,0,0,0]
+
+    This is really just building a decimal adder.
+    We can do this in O(n) time and O(1) space.
+    result = copy of input reversed
+    for each index in result
+        increment the digit
+        set result value to new value mod 10
+        if the digit is < 10 we break, plus set a flag
+    if not flag:
+        add a '1' to the result
+    return reversed result
+    O(n) time
+    O(1) space unless we reverse with a copy...
+
+    Finished: 15:52
+    Pretty good...  Very easy question.
+
+    Had one bug.  I didn't reverse the result I just copied it.  Weird, don't know why I did that.
+    Also, I learned that you can reverse in place with array.reverse()
+    """
+    result = A[::-1]
+    final_carry = True
+    for i, val in enumerate(result):
+        incremented = val + 1
+        result[i] = incremented % 10
+        if incremented < 10:
+            final_carry = False
+            break
+    if final_carry:
+        result.append(1)
+    result.reverse()
+    return result
+
 
 
 if __name__ == '__main__':
