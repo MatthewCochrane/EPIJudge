6129b3973ba126fe6de62a5d7c7ad26cb76b211f
Matthew Cochrane <matthew.cochrane.eng@gmail.com>
2022-03-18 11:11:50 +1000

    update comments

--- epi_judge_python/is_string_palindromic.py
+++ epi_judge_python/is_string_palindromic.py
@@ -2,7 +2,10 @@ from test_framework import generic_test
 
 
 def is_palindromic(s: str) -> bool:
+    # note that this is faster because it's all done in c
     return s == s[::-1]
+    # this is O(1) space
+    return all(s[i] == s[~i] for i in range(len(s) // 2))
 
 
 if __name__ == '__main__':
