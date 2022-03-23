6582b8a67caaa6ee426a2b321c87c526c41fe1cc
Matthew Cochrane <matthew.cochrane.eng@gmail.com>
2022-03-23 18:13:00 +1000

    7.12

--- epi_judge_python/pivot_list.py
+++ epi_judge_python/pivot_list.py
@@ -7,9 +7,103 @@ from test_framework.test_failure import TestFailure
 from test_framework.test_utils import enable_executor_hook
 
 
-def list_pivoting(l: ListNode, x: int) -> Optional[ListNode]:
-    # TODO - you fill in here.
-    return None
+def list_pivoting(head: Optional[ListNode], k: int) -> Optional[ListNode]:
+    """
+    Start: 17:33
+    Pivot about the value k
+    Example
+        1,2,6,4,2,2,3,4,5,3, k=3
+        1,2,2,2,3,3,6,4,4,5
+
+    How I did this in my head:
+        three passes
+        pass 1 -> find elements < k
+        pass 2 -> find elements == k
+        pass 3 -> find elements > k
+
+    We could do this all in one pass with three separate lists
+        o-> k=3
+        l->1->2->2->2
+        e->3->3
+        g->6->4->4->5
+
+        join l->e->g
+
+    Pseudo code
+    orig_dummy point at head
+    less_dummy point at None, less_tail
+    eq_dummy point at None, eq_tail
+    more_dummy point at None, more_tail
+    while orig_dummy.next:
+        cur = orig_dummy.next
+        if cur data < pivot:
+            point less_tail at cur
+            set less_tail to cur
+        elif cur data is pivot:
+            point eq_tail to cur
+            set eq_tail to cur
+        else:
+            point more_tail to cur
+            set more_tail to cur
+    result_dummy = less_dummy
+    less_tail.next = eq_dummy.next
+    eq_tail.next = more_dummy.next
+
+    d->
+    d->e
+    d->g->g->g
+
+    Join three linked lists that may be empty
+    head, tail = None, None
+    for (pd, pt) in [(ld, lt), (ed, et), (md, mt)]:
+        if pd.next is None:
+            continue
+        if head is None:
+            head = pd.next
+        else:
+            tail.next = pd.next
+        tail = pt
+    tail.next = None
+    return head
+
+    Time: O(n)
+    Space: O(1)
+
+    Finished: 18:12
+    About 45 mins.  Too slow really.  Decent but could be better.
+    Had two very small bugs.
+    """
+    cur = head
+    less_dummy = less_tail = ListNode()
+    eq_dummy = eq_tail = ListNode()
+    more_dummy = more_tail = ListNode()
+    while cur:
+        if cur.data < k:
+            less_tail.next = cur
+            less_tail = cur
+        elif cur.data == k:
+            eq_tail.next = cur
+            eq_tail = cur
+        else:
+            more_tail.next = cur
+            more_tail = cur
+        cur = cur.next
+    head, tail = None, None
+    for (pd, pt) in [
+        (less_dummy, less_tail),
+        (eq_dummy, eq_tail),
+        (more_dummy, more_tail),
+    ]:
+        if pd.next is None:
+            continue
+        if head is None:
+            head = pd.next
+        else:
+            tail.next = pd.next
+        tail = pt
+    if tail:
+        tail.next = None
+    return head
 
 
 def linked_to_list(l):
@@ -36,18 +130,20 @@ def list_pivoting_wrapper(executor, l, x):
                 mode = 1
         elif mode == 0:
             if i < x:
-                raise TestFailure('List is not pivoted')
+                raise TestFailure("List is not pivoted")
             elif i > x:
                 mode = 1
         else:
             if i <= x:
-                raise TestFailure('List is not pivoted')
+                raise TestFailure("List is not pivoted")
 
     if sorted(original) != sorted(pivoted):
-        raise TestFailure('Result list contains different values')
+        raise TestFailure("Result list contains different values")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     exit(
-        generic_test.generic_test_main('pivot_list.py', 'pivot_list.tsv',
-                                       list_pivoting_wrapper))
+        generic_test.generic_test_main(
+            "pivot_list.py", "pivot_list.tsv", list_pivoting_wrapper
+        )
+    )
