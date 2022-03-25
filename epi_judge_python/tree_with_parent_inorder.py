3997c5f510720ca8729c3a16162547bbbce47fd0
Matthew Cochrane <matthew.cochrane.eng@gmail.com>
2022-03-25 15:47:51 +1000

    9.11 - 9.10 in the repo index

--- epi_judge_python/tree_with_parent_inorder.py
+++ epi_judge_python/tree_with_parent_inorder.py
@@ -1,16 +1,29 @@
 from typing import List
 
 from binary_tree_with_parent_prototype import BinaryTreeNode
+from successor_in_tree import find_successor
 from test_framework import generic_test
 
 
 def inorder_traversal(tree: BinaryTreeNode) -> List[int]:
-    # TODO - you fill in here.
-    return []
+    # Kinda cheated.. Reused the previous question's answer.
+    if not tree:
+        return []
+    result = []
+    node = tree
+    while node.left:
+        node = node.left
+    while node:
+        result.append(node.data)
+        node = find_successor(node)
+    return result
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     exit(
-        generic_test.generic_test_main('tree_with_parent_inorder.py',
-                                       'tree_with_parent_inorder.tsv',
-                                       inorder_traversal))
+        generic_test.generic_test_main(
+            "tree_with_parent_inorder.py",
+            "tree_with_parent_inorder.tsv",
+            inorder_traversal,
+        )
+    )
