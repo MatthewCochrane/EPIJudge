8edf0f7982d4c2d627a38cc51553cab0ce1efe38
Matthew Cochrane <matthew.cochrane.eng@gmail.com>
2022-04-24 09:31:21 +1000

    13.1

--- epi_judge_python/intersect_sorted_arrays.py
+++ epi_judge_python/intersect_sorted_arrays.py
@@ -4,8 +4,64 @@ from test_framework import generic_test
 
 
 def intersect_two_sorted_arrays(A: List[int], B: List[int]) -> List[int]:
-    # TODO - you fill in here.
-    return []
+    """
+    Find the intersection of two arrays.
+    Example:
+        A: 1,2,3,3,4,6,8,9,9
+        B: 3,4,10,12
+        Result: 3,4
+
+    We can do this by converting each to a set then using intersection.
+
+    That takes O(n + m) time and O(n) space.
+
+    We can improve this by using a pointer-based approach which should be able to do it in
+    O(n + m) time and O(1) space.
+
+    If we iterate a pointer over each input list, increment the pointer with the smaller value.  If the
+    values are the same then add it to the result (if it's not already the last item in the result).
+
+        A: 1,2,3,3,4,6,8,9,9
+                            |
+        B: 3,4,10,12
+               |
+
+        Result: 3,4
+
+    Pseudo code
+    b_idx = 0
+    result = []
+    for val_a in A:
+        while b_idx < len(B) and val_b < val_a:
+            b_idx += 1
+        if we go off the end of b:
+            break
+        if val_a == val_b:
+            if not equal to last result (and last result exists), add to results
+        # else -> val_a < val_b... do nothing, increment a_idx
+    return result
+
+    Done.
+    Solution 2 takes quite a bit more time than solution 1 because it's all done in python
+    whereas solution 1 does a lot in c.
+    Still, solution 2 has equal time complexity to solution 1 and better space complexity.
+
+    """
+    # solution 2
+    b_idx = 0
+    result = []
+    for val_a in A:
+        while b_idx < len(B) and B[b_idx] < val_a:
+            b_idx += 1
+        if b_idx >= len(B):
+            break
+        if val_a == B[b_idx]:
+            if not result or result[-1] != val_a:
+                result.append(val_a)
+    return result
+    # solution 1
+    #return list(sorted(set(A).intersection(B)))
+
 
 
 if __name__ == '__main__':
--- epi_judge_python/power_set.py
+++ epi_judge_python/power_set.py
@@ -4,12 +4,70 @@ from test_framework import generic_test, test_utils
 
 
 def generate_power_set(input_set: List[int]) -> List[List[int]]:
-    # TODO - you fill in here.
-    return []
+    """
+    Start: 16:33
+    Return the set of all subsets of S.
+    Remember that there are 2^n subsets of a set S of length n.
+    We can generate this in different ways.
+    In essence every number in S is either included or not.  That gives n binary choices
+    and 2^n results.
+    One way to do this is to just enumerate the numbers from 0 to 2^n, then for each bit
+    chose whether to include that value from S in the result set.
+    We can also solve it recursively.
+    We either chose to include S[0] or not, then for each of those choices we include the powerset of S[1:]
+
+    iterative:
+    for i in range(int(2**n)):
+        results.append(list(filter(lambda j: (1<<j) & i, range(n))))
+
+    recursive:
+    result = []
+    results = []
+    def backtrack(start_idx):
+        if start_idx == len(nums):
+            results.append([*result])]
+            return
+        backtrack(start_idx + 1)
+        result.append(nums[start_idx])
+        backtrack(start_idx + 1)
+        result.pop()
+    backtrack(0)
+    return result
+
+    Finish 16:46
+    """
+    # 75 us
+    result = []
+    results = []
+
+    def backtrack(start_idx):
+        if start_idx == len(input_set):
+            results.append([*result])
+            return
+        backtrack(start_idx + 1)
+        result.append(input_set[start_idx])
+        backtrack(start_idx + 1)
+        result.pop()
+
+    backtrack(0)
+    return results
+
+    # 248 ms
+    n = len(input_set)
+    results = []
+    for i in range(int(2 ** n)):
+        results.append(
+            list(map(lambda x: input_set[x], filter(lambda j: (1 << j) & i, range(n))))
+        )
+    return results
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     exit(
-        generic_test.generic_test_main('power_set.py', 'power_set.tsv',
-                                       generate_power_set,
-                                       test_utils.unordered_compare))
+        generic_test.generic_test_main(
+            "power_set.py",
+            "power_set.tsv",
+            generate_power_set,
+            test_utils.unordered_compare,
+        )
+    )
