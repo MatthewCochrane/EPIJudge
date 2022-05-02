5a9f35a78f0a4d748fdabc60c2601636426dade7
Matthew Cochrane <matthew.cochrane.eng@gmail.com>
2022-05-02 11:01:52 +1000

    15.6 and 15.7

--- epi_judge_python/combinations.py
+++ epi_judge_python/combinations.py
@@ -4,14 +4,115 @@ from test_framework import generic_test, test_utils
 
 
 def combinations(n: int, k: int) -> List[List[int]]:
-    # TODO - you fill in here.
-    return []
+    """
+    Return all size k subsets from 1..n
+    Eg.
+    n = 3, k = 2
+        full set is {1,2,3}
+        return [{1,2}, {1,3}, {2,3}]
+    there are n bits and we want all of the numbers up to 2^n that have k set bits.
+    011, 101, 110
+    or for a larger number
+    43210
+    0001?
+    00011
+
+    001??
+    00101
+    00110
+
+    01???
+    01001
+    01010
+    01100
+
+    1????
+    10001
+    10010
+    10100
+    11000
+
+    00111
+    01011
+    01101
+    01110
+    10011
+
+    result = []
+    def comb(n, k, num):
+        if k == 0:
+            add to result
+        for i in range(k-1, n):
+            comb(i, k-1, num | (1<< i))
+    comb(n, k)
+    return result
 
 
-if __name__ == '__main__':
+    func comb(n, k):
+        if hit bottom:
+            add to result
+        loop over indices from k to n:
+            set a bit
+            recurse(subproblem of bits to the right of the set bit, k-1)
+    call comb
+    return results
+
+    func comb()
+        recurse()
+
+
+
+
+
+
+
+    put a 1 somewhere, then select k-1 from the numbers to the right
+    eg. c(5, 3) ->
+        set bit 2, c(2, 2)
+        set bit 3, c(3, 2)
+        set bit 4, c(4, 2)
+
+    def comb(n, k, num):
+        if k == 0:
+            add to result
+        for leftmost in range(k-1, n):
+            num |= (1 << leftmost)
+            comb(leftmost, k-1)
+            num &= ~(1 << leftmost)
+
+    c(5, 3)
+        for l in range(3-1, 5):
+            comb(2, 2)
+                for l in range(1, 2):
+                    comb(1, 1)
+                        for l in range(0, 1):
+                            comb(0,0)
+            comb(3, 2)
+            comb(4, 2)
+
+
+    """
+    result = []
+
+    def comb(n, k, num):
+        if k == 0:
+            result.append([i + 1 for i, v in enumerate(bin(num)[2:][::-1]) if v == '1'])
+            return
+        for leftmost in range(k - 1, n):
+            comb(leftmost, k - 1, num | (1 << leftmost))
+
+    comb(n, k, 0)
+    return result
+
+
+if __name__ == "__main__":
+    # print(combinations(5, 2))
+    # exit(0)
     exit(
         generic_test.generic_test_main(
-            'combinations.py',
-            'combinations.tsv',
+            "combinations.py",
+            "combinations.tsv",
             combinations,
-            comparator=test_utils.unordered_compare))
+            comparator=test_utils.unordered_compare,
+        )
+    )
--- epi_judge_python/enumerate_palindromic_decompositions.py
+++ epi_judge_python/enumerate_palindromic_decompositions.py
@@ -4,17 +4,104 @@ from test_framework import generic_test
 
 
 def palindrome_decompositions(text: str) -> List[List[str]]:
-    # TODO - you fill in here.
-    return []
+    """
+    Compute all palindromic decompositions of text.
+
+    "01234"
+    return [["0", "1", "2", "3", "4"]]
+
+    "010123"
+    [0, 1, 0, 1, 2, 3], [010, 1, 2, 3], [0, 101, 2, 3]
+
+    "010111"
+    [0 1 0 1 1 1], [010, 111], [010, 11, 1], [010, 1, 11], [0, 101, 1, 1], [0, 101, 11]
+
+    "010111"
+    0 10111
+      1 0111
+        0 111
+          1 11
+            1 1 - 0 1 0 1 1 1
+            11 - 0 1 0 1 11
+          111 - 0 1 0 111
+          11 1 - 0 1 0 11 1
+      101 11
+        1 1 - 0 101 1 1
+        11 - 0 101 11
+    010 111
+      1 11
+        1 1 - 010 1 1 1
+        11 - 010 1 11
+      11 1 - 010 11 1
+
+    def find_palindromes(txt):
+        find palindrome prefix's:
+            if suffix is not empty:
+                results = find_palindromes(bit after prefix)
+            add prefix to each result or just add suffix
+
+    prefix = []
+    def find_palindromes(start_idx):
+        if at end of str:
+            copy prefix to results
+        find palindrome prefix's:
+            add prefix to prefix list as a group
+            find_palindromes(new_suffix)
+            pop prefix
+    find_palindromes(0)
+
+
+
+
+    len = 6
+    "010123"
+    fp(0)
+        for i in range(0, 6):
+            i=0
+            0 == 0
+            prefix = [0]
+            fp(1)
+            for i in range(1, 6):
+                i=1
+                1 == 1
+                ...
+            i=1
+            01 != 10
+            i=2
+            010 == 010
+            prefix = [010]
+            fp(3)
+            ...
+
+
+    """
+    result = []
+    prefix = []
+
+    def find_palindromes(start_idx):
+        if start_idx >= len(text):
+            result.append([*prefix])
+        for i in range(start_idx, len(text)):
+            slice = text[start_idx : i + 1]
+            if slice == slice[::-1]:  # is palindromic
+                prefix.append(slice)
+                find_palindromes(i + 1)
+                prefix.pop()
+
+    find_palindromes(0)
+    return result
 
 
 def comp(a, b):
     return sorted(a) == sorted(b)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     exit(
         generic_test.generic_test_main(
-            'enumerate_palindromic_decompositions.py',
-            'enumerate_palindromic_decompositions.tsv',
-            palindrome_decompositions, comp))
+            "enumerate_palindromic_decompositions.py",
+            "enumerate_palindromic_decompositions.tsv",
+            palindrome_decompositions,
+            comp,
+        )
+    )
